89a6adf512da7e37c135b1cfddbc2d7b2fc3c589
kaynanguarany <kayna@guarany.net>
2017-08-23 07:35:25 -0300

    Config redux (#1)
    * Config redux using Provider component in index.js
    *  Create connection between actions and redux states with Accounts component

---
+++ src/actions/contants.js
@@ -0,0 +1,6 @@
+export const FINANCE_LIST = [
+  {id: 1, name: "Kaynã", initial_balance: "100.00"},
+  {id: 2, name: "Ary", initial_balance: "1.00"},
+  {id: 3, name: "Fábio", initial_balance: "1.00"},
+  {id: 4, name: "Marcelo", initial_balance: "1.00"},
+];
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import FinanceReducer from './reducers/FinanceReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = combineReducers({ finance: FinanceReducer });
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ), 
+  document.getElementById('root')
+);
 registerServiceWorker();
